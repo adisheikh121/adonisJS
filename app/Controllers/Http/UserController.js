da876df4f689330697e476463b4279058d79e262
JOAO_VICTOR <joaovictormendesavila@gmail.com>
2020-03-29 13:13:40 -0300

    Configuração inicial.

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const User = use("App/Models/User");
+const Database = use("Database");
+
+class UserController {
+  async store({ request }) {
+    const data = request.only(["username", "email", "password"]);
+
+    const adresses = request.input("adresses");
+
+    const trx = await Database.beginTransaction();
+
+    const user = await User.create(data, trx);
+
+    await user.adresses().createMany(adresses, trx);
+
+    await trx.commit();
+
+    return user;
+  }
+}
+
+module.exports = UserController;
